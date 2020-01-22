05c9314f5d986d31e165c73bd6a8281a29f66057
root <root@localhost.localdomain>
2020-01-22 09:49:00 +0100

    ajout structure docker-compose.yml + mise à jour creation.sql, affichage.php.j2

--- httpd/webfiles/creation.sql
+++ httpd/webfiles/creation.sql
@@ -28,20 +28,6 @@ ALTER TABLE `tgv`
 ALTER TABLE `tgv` ADD FOREIGN KEY (`id_gare_depart`) REFERENCES gares(`id_gare`);
 ALTER TABLE `tgv` ADD FOREIGN KEY (`id_gare_arrivee`) REFERENCES gares(`id_gare`);
 
-INSERT INTO `tgv` VALUES
-( 1, '2020-01-01 07:00:00','2020-01-02 06:00:00', 10 , 20), 
-( 2, '2020-11-12 17:00:00','2020-11-12 18:00:00', 30 , 10),
-( 3, '2020-08-27 06:00:00','2020-08-27 11:00:00', 30 , 20),
-( 4, '2020-02-25 11:00:00','2020-02-27 08:00:00', 40 , 50),
-( 5, '2020-05-07 14:00:00','2020-05-08 07:00:00', 20 , 50);
-
-INSERT INTO `gares` VALUES
-(10,'Lyon'),
-(20,'Chambery'),
-(30,'Paris'),
-(40,'Toulouse'),
-(50,'Pau');
-
 
 
 
